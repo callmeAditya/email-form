d0671ae83344814c1b92491901c05a095c24f1ce
Aditya Rana <aditya.rana@truminds.com>
2021-12-27 12:12:44 +0530

    email

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,24 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'email';
+  export class AppComponent {
+    email:string='';
+    text='text!'
+    
+    test='abcdefghijklmnopqrstuvwxyz';
+
+    constructor(){
+    // let i=0;        
+    //   setInterval(()=>{
+    //     if(i===26) return;
+    //     this.email+=this.test.charAt(i)
+    //     i++;
+
+    //   },500)
+    }
+
+    onSubmit(){
+      console.log('Form has been submitted using email: ',this.email);
+      
+    }
 }
